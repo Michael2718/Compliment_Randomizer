3c7ec6e9d7fa46471635d7a518b49abbb3bed3be
Michael2718 <isaevmihail007@gmail.com>
2020-11-17 21:01:54 +0200

    Oh man we have a bug with fuckin russian grammar

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import random
 file = open('adjectives.txt', mode='r', encoding='utf8').read()
 arr_of_adjectives = file.split("\n")
 random_adjective = arr_of_adjectives[random.randrange(0, len(arr_of_adjectives))]
-print("Спокойной ночи, мои {}❤".format(random_adjective[:-2] + 'ые'))  # для множественного числа
+print("Спокойной ночи, мои {}❤".format(random_adjective[:-2] + 'ые'))  # здесь есть баг с окончанием в множественном числе
 print("Спокойной ночи, моя {}❤".format(random_adjective))
