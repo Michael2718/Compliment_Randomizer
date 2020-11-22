24ac123666747ca080c9ad6b77a126141e47927f
Michael2718 <isaevmihail007@gmail.com>
2020-11-22 14:52:36 +0200

    Add docs for is_hard_consonant and generate_compliment functions. Major improvements.

--- main.py
+++ main.py
@@ -2,23 +2,40 @@ import random
 
 
 def is_hard_consonant(c):
+    """
+    Returns True if the russian letter c is hard consonant(according to the russian grammar).
+
+    Parameters:
+        c (str):The letter to be checked.
+    Returns:
+        bool:True if c is hard hard consonant or false if it is not.
+    """
     hard_letters = ['п', 'б', 'ф', 'в', 'т', 'д', 'с', 'з', 'ц', 'м', 'н', 'р', 'л']
     return c in hard_letters
 
 
 def generate_compliment(array, mode):
-    random_adjective = array[random.randrange(0, len(array))]
+    """
+    Returns the random compliment(adjective then) from an array and formats it according to mode.
+    Formatting works only with russian adjectives so far.
+
+    Parameters:
+        array (list):The list of adjectives.
+        mode (str):The adjective formatting mode, should equal 'single' or 'plural'.
+    Returns:
+        str:The random adjective from an array in the right form, specified by mode.
+    """
+    random_adjective = array[random.randrange(len(array))]
     if mode == 'single':
-        return "Спокойной ночи, моя {}❤".format(random_adjective)
+        return random_adjective
     elif mode == "plural":
         if is_hard_consonant(random_adjective[-3]) and random_adjective[-2] != 'я':
-            return "Спокойной ночи, мои {}❤".format(random_adjective[:-2] + 'ые')
+            return random_adjective[:-2] + 'ые'
         else:
-            return "Спокойной ночи, мои {}❤".format(random_adjective[:-2] + 'ие')
+            return random_adjective[:-2] + 'ие'
 
 
 if __name__ == "__main__":
     file = open('adjectives.txt', mode='r', encoding='utf8').read()
     arr_of_adjectives = file.split("\n")
-    for x in range(100):
-        print(generate_compliment(arr_of_adjectives, mode='plural'))
+    print("Спокойной ночи, моя {}❤".format(generate_compliment(arr_of_adjectives, mode='single')))  # Usage example
